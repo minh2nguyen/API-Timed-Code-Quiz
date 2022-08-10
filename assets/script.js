14443664215c960501a845150c61cefb042c3dc0
MinhThu <mttnguyen91@gmail.com>
2022-08-10 17:24:31 -0500

    Editing Start button in JS

---
+++ assets/script.js
@@ -0,0 +1,21 @@
+//  Declare global variables
+var startBtn = $("#startBtn");
+var timer = $("#timer");
+var startTime = false;
+var remainTime = true;
+
+var choiceA = $("#choiceA");
+var choiceB = $("#choiceB");
+var choiceC = $("#choiceC");
+var choiceD = $("#choiceD");
+
+
+// Countdown Timer function
+function coutdownTimer(){
+}
+
+// Click Start Button 
+$("#startBtn").on("click",function(){
+
+});
+console.log("start!")

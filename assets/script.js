347689d0c9d08bec5a4a7c66efdefad0512e3a4f
MinhThu <mttnguyen91@gmail.com>
2022-08-10 23:35:01 -0500

    Setup timer in JS

--- assets/script.js
+++ assets/script.js
@@ -1,60 +1,67 @@
 //  Declare global variables
 // var startBtn = $("#startBtn");
-var timer = $("#startBtn");
-var score = 0;
-var questionIndex = 0;
-
+// var timer = $("#startBtn");
+// var score = 0;
+// var questionIndex = 0;
 
 
 // Questions arrays
 var questions = [
-    { 
+    {
         question: "The condition in an if / else statement is enclosed within ____.",
         choices:["quotes", "curly brackets", "parentheses", "square brackets"],
-        answer: "parenthese"
-    },
+        corAnswer: "parenthese"
+    }, 
     {
         question: "Commonly used data types DO NOT include:",
         choices: ["strings", "booleans", "alerts", "numbers"],
-        answer: "alerts"
+        corAnswer: "alerts"
     },
     {
         question: "Arrays in Javascript can be used to store ____.",
         choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
-        answer: "all of the above"
-    },
+        corAnswer: "all of the above"
+    }, 
     {
         question: "String values must be enclosed within ____ when being assigned to variables.",
         choices: ["commas", "curly brackets", "quotes", "parenthesis"],
-        answer: "quotes"
+        corAnswer: "quotes"
     },
     {
-        title: "A very useful tool for used during development and debugging for printing content to the debugger is:",
+        question: "A very useful tool for used during development and debugging for printing content to the debugger is:",
         choices: ["Javascript", "terminal / bash", "for loops", "console log"],
         answer: "console log"
-    },
-]
+    }];
 
 // Seconds in timer
 var seconds = 75;
 var timeInterval = 0;
-var currentTime = $("#timer");
-
+var penaltyTime = 10;
+var timer = document.querySelector("#startBtn");
+var time = document.querySelector("#timer");
 // Countdown Timer function
-timer.addEventListener("click".function()) {
-    if (timeInterval === 0) {
-        timeInterval = setInterval(function(){
-            seconds--;
-            currentTime.textContent = seconds;
-        })
-    }
-}
+// timer.addEventListener("click".function()) {
+//     if (timeInterval === 0) {
+//         timeInterval = setInterval(function(){
+//             seconds--;
+//             currentTime.textContent = seconds;
+//         })
+//     }
+// }
 
 // Click Start Button 
-$("#startBtn").on("click",function(event){
-    event.preventDefault();
+timer.addEventListener("click", function(){
+    if (timeInterval === 0){
+        timeInterval = setInterval(function() {
+            seconds--;
+            time.textContent = seconds;
+        })
+
+    }
+
+
 });
-console.log("START QUIZ")
+
 // var answerChoices = $("#answer-choices");
 // var answer = answerChoices.val();
 // console.log(answer)

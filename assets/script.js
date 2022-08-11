e3043addcc06da9d97fc7ddf4c831a9b2831e32e
MinhThu <mttnguyen91@gmail.com>
2022-08-10 22:29:49 -0500

    Start styling in CSS

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,42 @@
 //  Declare global variables
-var startBtn = $("#startBtn");
-var timer = $("#timer");
-var startTime = false;
-var remainTime = true;
+// var startBtn = $("#startBtn");
+var timer = $("#startBtn");
+var score = 0;
+var questionIndex = 0;
 
-var choiceA = $("#choiceA");
-var choiceB = $("#choiceB");
-var choiceC = $("#choiceC");
-var choiceD = $("#choiceD");
 
+
+// Questions arrays
+var questions = [
+    { 
+    question: "The condition in an if / else statement is enclosed within ____.",
+    choices:["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "parenthese"
+    },
+]
+
+// Seconds in timer
+var seconds = 75;
+var timeInterval = 0;
+var currentTime = $("#timer");
 
 // Countdown Timer function
-function coutdownTimer(){
+timer.addEventListener("click".function()) {
+    if (timeInterval === 0) {
+        timeInterval = setInterval(function(){
+            seconds--;
+            currentTime.textContent = seconds;
+        })
+    }
 }
 
 // Click Start Button 
-$("#startBtn").on("click",function(){
-
+$("#startBtn").on("click",function(event){
+    event.preventDefault();
 });
-console.log("start!")
+console.log("START QUIZ")
+// var answerChoices = $("#answer-choices");
+// var answer = answerChoices.val();
+// console.log(answer)
+
+

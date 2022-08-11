923fba893ccb617ebd472d624a04ad2579c0c26f
MinhThu <mttnguyen91@gmail.com>
2022-08-11 14:23:36 -0500

    Attempted to set up question function in JS

--- assets/script.js
+++ assets/script.js
@@ -6,12 +6,12 @@
 
 
 // Questions arrays
-var questions = [
+var questionsArray = [
     {
         question: "The condition in an if / else statement is enclosed within ____.",
-        choices:["quotes", "curly brackets", "parentheses", "square brackets"],
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
         corAnswer: "parenthese"
-    }, 
+    },
     {
         question: "Commonly used data types DO NOT include:",
         choices: ["strings", "booleans", "alerts", "numbers"],
@@ -21,7 +21,7 @@ var questions = [
         question: "Arrays in Javascript can be used to store ____.",
         choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
         corAnswer: "all of the above"
-    }, 
+    },
     {
         question: "String values must be enclosed within ____ when being assigned to variables.",
         choices: ["commas", "curly brackets", "quotes", "parenthesis"],
@@ -33,34 +33,54 @@ var questions = [
         answer: "console log"
     }];
 
+// Declared var
+var score = 0;
+var questionChoices = 0;
+var openScreen = document.querySelector("open-screen");
+
 // Seconds in timer
 var seconds = 75;
 var timeInterval = 0;
 var penaltyTime = 10;
 var timer = document.querySelector("#startBtn");
 var time = document.querySelector("#timer");
-// Countdown Timer function
-// timer.addEventListener("click".function()) {
-//     if (timeInterval === 0) {
-//         timeInterval = setInterval(function(){
-//             seconds--;
-//             currentTime.textContent = seconds;
-//         })
-//     }
-// }
+var create = document.createElement("ul");
 
-// Click Start Button 
-timer.addEventListener("click", function(){
-    if (timeInterval === 0){
-        timeInterval = setInterval(function() {
+// Click Start Button to activate countdown timer 
+timer.addEventListener("click", function () {
+    if (timeInterval === 0) {
+        timeInterval = setInterval(function () {
             seconds--;
             time.textContent = seconds;
-        })
 
+            if (seconds <= 0) {
+                clearInterval(timeInterval);
+                allDone();
+                time.textContent = "Time's up!";
+            }
+        },1000);
+    }
+    render(questionChoices);
+});
+
+// Function that asks the quesitons and displays the choices 
+function render(questionChoices) {
+    openScreen.innerHTML= "";
+    create.innerHTML = "";
+    for (var i = 0; i < questionsArray.length; i++) {
+        var questionsAsk = questionsArray(questionChoices).question;
+        var answerOptions = questionsArray(questionChoices).choices;
+        openScreen.textContent = questionsAsk;
     }
 
-
-});
+    answerOptions.forEach(function (newItem) {
+        var listItem = document.createElement("li");
+        listItem.textContent = newItem;
+        openScreen.appendChild(create);
+        create.appendChild(listItem);
+        listItem.addEventListener("click", (compare));
+    })
+}
 
 // var answerChoices = $("#answer-choices");
 // var answer = answerChoices.val();
